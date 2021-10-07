b2a0fedc49007a5ef2bf79e5e4b4acfe64494e0f
zabuss <90361221+zabuss@users.noreply.github.com>
2021-10-07 18:37:48 -0500

    use variable

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 function bottomBore() {
+     var b_bore_diameter =  document.getElementById("text").value
+   document.getElementById("ascii").value = b_bore_diameter;
 }
 function topBore() {
- topboredia = "";
+    var t_bore_diameter =  document.getElementById("text").value
+   document.getElementById("ascii").value = t_bore_diameter;
 }
-
